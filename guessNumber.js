85f36c37688d014e2f434ca4d80d7d2b620034ab
Haider <haider.2002.786@gmail.com>
2023-05-11 23:27:11 +0500

    first commit

---
+++ guessNumber.js
@@ -0,0 +1,37 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+import chalkAnimation from "chalk-animation";
+const sleep = () => {
+    return new Promise((res) => {
+        setTimeout(res, 2000);
+    });
+};
+async function welcome() {
+    let rainbow = chalkAnimation.rainbow("lets start the Game:"); // start
+    await sleep();
+    rainbow.stop();
+}
+let count = 0;
+async function game() {
+    let answers;
+    do {
+        answers = await inquirer
+            .prompt([
+            {
+                name: "num",
+                type: "number",
+                message: "Computer have numbers from 0 to 100.\nEnter your number between them:"
+            }
+        ]);
+        count++;
+        if (answers.num == a)
+            console.log(chalk.green("Excellent you guess the number in", count, "attempts."));
+        else if (answers.num > a)
+            console.log(chalk.yellow("Try again,your guess number is greater."));
+        else if (answers.num < a)
+            console.log(chalk.yellow("Try again,your guess number is lesser."));
+    } while (answers.num != a);
+}
+const a = Math.floor(Math.random() * 100);
+await welcome();
+await game();
